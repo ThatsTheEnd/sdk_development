f8d0c1140bd2f21a2512e73c4524bb0c8d41b050
Nikolaus Naredi-Rainer <nikolaus.naredi-rainer@zuehlke.com>
2023-09-06 14:53:43 +0200

    update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,14 @@ author = "Niko Naredi"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.autodoc',
+    'sphinx.ext.coverage',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode']
+
+napoleon_google_docstring = True
+napoleon_use_param = False
+napoleon_use_ivar = True
 
 templates_path = ["_templates"]
 exclude_patterns = []
